d1db5ce2f069a175877f949a9617fa331b65316a
qinhan <1036474541@qq.com>
2017-07-08 13:43:44 +0800

    add

---
+++ day_contact/finally.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+import os,sys,atexit,time
+try:
+    pass
+    while(True):
+        pass
+except Exception as e:
+    pass
+finally:
+    with open('./test.txt','a+') as fn:
+        fn.write("123\n")
