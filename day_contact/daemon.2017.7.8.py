5930f3f54549fe7836ce99f3ad51c8cc61878dd1
qinhan <1036474541@qq.com>
2017-07-08 12:45:41 +0800

    add

--- day_contact/daemon.2017.7.8.py
+++ day_contact/daemon.2017.7.8.py
@@ -16,16 +16,17 @@ class daemon:
         pid = os.fork()
         if pid:
             sys.exit(0)
+        print(os.getpid())
         sys.stderr.flush()
         sys.stdout.flush()
-        with open('/dev/null') as read_null,open('/dev/null','w') as write_null:
-            os.dup2(read_null.fileno(),sys.stdin.fileno())
-            os.dup2(write_null.fileno(), sys.stdout.fileno())
-            os.dup2(write_null.fileno(), sys.stderr.fileno())
-        if self.pid_file:
-            with open(self.pid_file,'w') as fn:
-                fn.write(str(os.getpid()))
-            atexit.register(os.remove,self.pid_file)
+        # with open('/dev/null') as read_null,open('/dev/null','w') as write_null:
+        #     os.dup2(read_null.fileno(),sys.stdin.fileno())
+        #     os.dup2(write_null.fileno(), sys.stdout.fileno())
+        #     os.dup2(write_null.fileno(), sys.stderr.fileno())
+        # if self.pid_file:
+        #     with open(self.pid_file,'w') as fn:
+        #         fn.write(str(os.getpid()))
+        #     atexit.register(os.remove,self.pid_file)
         while(True):
             with open('./tmp/python.txt','aw') as fn:
                 fn.write(str(time.time()))
