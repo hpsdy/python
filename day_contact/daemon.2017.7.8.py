af5317456bbcf09db9de253c07432f1476b235bd
qinhan <1036474541@qq.com>
2017-07-08 13:07:43 +0800

    add

--- day_contact/daemon.2017.7.8.py
+++ day_contact/daemon.2017.7.8.py
@@ -7,30 +7,33 @@ class daemon:
         self.task = task
         self.pid_file = pid_file
     def _daemon(self):
-        pid = os.fork()
-        if pid: #父进程
-            sys.exit(0)
-        os.chdir('/')
-        os.umask(0)
-        os.setsid()
-        pid = os.fork()
-        if pid:
-            sys.exit(0)
-        print(os.getpid())
-        sys.stderr.flush()
-        sys.stdout.flush()
-        with open('/dev/null') as read_null,open('/dev/null','w') as write_null:
-            os.dup2(read_null.fileno(),sys.stdin.fileno())
-            os.dup2(write_null.fileno(), sys.stdout.fileno())
-            os.dup2(write_null.fileno(), sys.stderr.fileno())
-        if self.pid_file:
-            with open(self.pid_file,'w') as fn:
-                fn.write(str(os.getpid()))
-            atexit.register(os.remove,self.pid_file)
-        while(True):
-            with open('./tmp/python.txt','a+') as fn:
-                fn.write(str(time.time())+"\n")
-            time.sleep(limit)
+        try:
+            pid = os.fork()
+            if pid: #父进程
+                sys.exit(0)
+            os.chdir('/')
+            os.umask(222)
+            os.setsid()
+            pid = os.fork()
+            if pid:
+                sys.exit(0)
+            print(os.getpid())
+            sys.stderr.flush()
+            sys.stdout.flush()
+            with open('/dev/null') as read_null,open('/dev/null','w') as write_null:
+                os.dup2(read_null.fileno(),sys.stdin.fileno())
+                os.dup2(write_null.fileno(), sys.stdout.fileno())
+                os.dup2(write_null.fileno(), sys.stderr.fileno())
+            if self.pid_file:
+                with open(self.pid_file,'w') as fn:
+                    fn.write(str(os.getpid()))
+                atexit.register(os.remove,self.pid_file)
+            while(True):
+                with open('./tmp/python.txt','a+') as fn:
+                    fn.write(str(time.time())+"\n")
+                time.sleep(limit)
+        finally:
+            os.remove(self.pid_file)
 if __name__=='__main__':
     mydaemon = daemon(pid_file='./tmp/python.pid')
     mydaemon._daemon()
